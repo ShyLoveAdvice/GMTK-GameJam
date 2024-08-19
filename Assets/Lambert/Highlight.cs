75c84baa21c4c1c2af140a4b3aeda6ffb578ef45
Paropb <paropbqiu@gmail.com>
2024-08-19 09:31:09 -0400

    updated ui:

--- Assets/Lambert/Highlight.cs
+++ Assets/Lambert/Highlight.cs
@@ -6,16 +6,57 @@ using UnityEngine.UI;
 public class Highlight : MonoBehaviour
 {
     public Material highlightMat;
+    public float mouseEnterHighlightWidth = 0.005f;
+    public float defaultHighlightWidth = 0f;
+    public float transitionDuration = 0.1f;
+    float targetHighlightWidth;
+    float targetHightlightSpeed;
+    float transitionTimer;
+    Material m_material;
+    bool transitioning;
     // Start is called before the first frame update
     void Start()
     {
-        highlightMat = GetComponent<Image>().material;
-        highlightMat.SetFloat("_OutlineWidth", Random.Range(0, 0.1f));
+        Image image = GetComponent<Image>();
+        image.material = new Material(highlightMat);
+        m_material = image.material;
+        //GetComponent<Image>().material.SetFloat("_OutlineWidth", Random.Range(0, 0.1f));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(transitioning)
+        {
+            if(transitionTimer < transitionDuration)
+            {
+                transitionTimer += Time.deltaTime;
+                m_material.SetFloat("_OutlineWidth", m_material.GetFloat("_OutlineWidth") + targetHightlightSpeed * Time.deltaTime);
+            }
+            else
+            {
+                m_material.SetFloat("_OutlineWidth", targetHighlightWidth);
+                transitioning = false;
+            }
+        }
+    }
+    public void MouseEnterHighlight()
+    {
+        Debug.Log("Mouse Enter");
+
+        var distance = mouseEnterHighlightWidth - m_material.GetFloat("_OutlineWidth");
+        targetHighlightWidth = mouseEnterHighlightWidth;
+        targetHightlightSpeed = distance / transitionDuration;
+        transitionTimer = 0f;
+
+        transitioning = true;
+    }
+    public void MouseExitHighlight()
+    {
+        var distance = defaultHighlightWidth - m_material.GetFloat("_OutlineWidth");
+        targetHighlightWidth = defaultHighlightWidth;
+        targetHightlightSpeed = distance / transitionDuration;
+        transitionTimer = 0f;
+
+        transitioning = true;
     }
 }
