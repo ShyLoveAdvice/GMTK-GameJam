24d25cdde050675638ec981bff545db72b62b78f
Paropb <paropbqiu@gmail.com>
2024-08-19 05:14:11 -0400

    complete :100:

--- Assets/Lambert/SceneManager.cs
+++ Assets/Lambert/SceneManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManager : PersistentSingleton<SceneManager>
+public class SceneManager : Singleton<SceneManager>
 {
     public string gameSceneName;
     public void LoadGameScene()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(gameSceneName);
+    }
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
